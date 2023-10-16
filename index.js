e4d282062ea5ccb69e1a8030f1b2cdd9cabb260a
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-10-16 14:08:41 +0530

    switch updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var button_list = document.querySelectorAll(".drum");
 
 for (var i = 0;i<button_list.length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        console.log(this);
+        // console.log(this);
         var buttonInnerHTML = this.innerHTML;
         switch (buttonInnerHTML) {
             case "w":
@@ -17,21 +17,26 @@ for (var i = 0;i<button_list.length;i++){
             case "s":
                 var tom3 = new Audio('sounds/tom-3.mp3');      
                 tom3.play();
+              break;
             case "d":
                 var tom4 = new Audio('sounds/tom-4.mp3');      
                 tom4.play();
+              break;    
             case "j":
                 var snare = new Audio('sounds/snare.mp3');      
                 snare.play();
+              break;  
             case "k":
                 var crash = new Audio('sounds/crash.mp3');      
                 crash.play();
+              break;
             case "l":
                 var kick = new Audio('sounds/kick-bass.mp3');      
                 kick.play();     
-                
+              break;  
             default:
-                break;
+                console.log("default")
+              break;
         }
     })
 }
